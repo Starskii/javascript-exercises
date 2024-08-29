d4c94050608435112386e0d77ff515872ed6ec7f
Starskii <jakeylakie@yahoo.com>
2024-08-28 23:38:49 -0700

    sol 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+    if (arguments.length >= 2){
+        for(let i = 1; i < arguments.length; i++){
+            for(let j = 0; j < arguments[0].length; j++){
+                console.log(`${arguments[i]} and ${arguments[0][j]}`);
+                console.log(arguments[i] === arguments[0][j]);
+                if(arguments[i] === arguments[0][j]){
+                    arguments[0].splice(j, 1);
+                    i--;
+                }
+            }
+        }
+    }else{
+        return;
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line

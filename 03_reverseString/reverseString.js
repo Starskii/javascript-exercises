28e8a1183250283d059a8aca27f8328e07184f4a
Starskii <jakeylakie@yahoo.com>
2024-08-28 22:54:27 -0700

    sol 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+    let reversedMessage = '';
+    for(let i = message.length; i >= 0; i--){
+        reversedMessage += message.charAt(i);
+    }
+    return reversedMessage;
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;

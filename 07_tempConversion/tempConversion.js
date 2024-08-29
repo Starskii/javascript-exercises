28365de5edd144d0daa107d832336e5a95ac0cd4
Starskii <jakeylakie@yahoo.com>
2024-08-29 00:17:07 -0700

    sol 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  return Math.round(((temperatureFahrenheit - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  return Math.round(((temperatureCelsius * (9/5)) + 32) * 10) / 10;
 };
-
 // Do not edit below this line
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 module.exports = {
   convertToCelsius,
   convertToFahrenheit

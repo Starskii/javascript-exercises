e3ca410ea40ba9c52f5914e801a77440436a47e9
Starskii <jakeylakie@yahoo.com>
2024-08-28 23:49:10 -0700

    sol 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numa, numb) {
+    if(!Number.isInteger(numa) || !Number.isInteger(numb))
+        return 'ERROR';
+    if(numa < 0 || numb < 0)
+        return 'ERROR';
+    let change = 1;
+    if(numa > numb)
+        change = -1;
+    let sum = 0;
+    while(numa != numb + change){
+        sum += numa;
+        numa += change;
+    }
+    return sum;
 };
 
 // Do not edit below this line

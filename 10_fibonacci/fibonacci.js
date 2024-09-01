0a6d1a724f0f2ccaa962df5c2fd087383b6bae18
Starskii <jakeylakie@yahoo.com>
2024-09-01 00:00:45 -0700

    sol 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let prev = 1;
+    let fib = 1;
+    n = parseInt(n);
+    if(n < 0)
+        return "OOPS";
+    if(n === 0)
+        return 0;
+    if(n === 1 || n === 2)
+        return 1;
 
+    for(let i = 2; i < n; i++){
+        let tmp = fib; 
+        fib += prev;
+        prev = tmp;
+    }
+    return fib;
 };
 
+console.log(fibonacci(3));
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const ageSort = (a, b) => {
+    let currentDate = new Date();
+    currentDate = currentDate.getFullYear();
+    if (a.yearOfDeath == null)
+        a.yearOfDeath = currentDate;
+    if (b.yearOfDeath == null)
+        b.yearOfDeath = currentDate;
+    a = a.yearOfDeath - a.yearOfBirth;
+    b = b.yearOfDeath - b.yearOfBirth;
+    if (a > b)
+        return -1;
+    if (a == b)
+        return 0;
+    if (a < b)
+        return 1;
+}
 
+const findTheOldest = function(people) {
+    return people.sort(ageSort)[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;

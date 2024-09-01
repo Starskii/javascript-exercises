b70f15a87b93812c4b44ba64ee7499873cafb751
Starskii <jakeylakie@yahoo.com>
2024-08-31 23:05:58 -0700

    sol 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    pal = pal.split('').filter((char) => {return /[a-zA-Z0-9]/.test(char)}).map((char) => {return char.toLowerCase()}).join('');
+    console.log(pal);
+    for(let i = 0; i < pal.length/2; i++){
+        if(pal.charAt(i) != pal.charAt(pal.length-(i+1)))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line

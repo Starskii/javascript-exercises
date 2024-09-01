4a8a0ba66fe06d8c5885fb69ebab20eac05ce0c5
Starskii <jakeylakie@yahoo.com>
2024-08-31 22:48:20 -0700

    sol 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0)
+    return 0;
+	return array.reduce(add);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, element) => {return total * element});
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	total = 1;
+  for (let i = a; i > 0; i--){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -31,3 +37,4 @@ module.exports = {
   power,
   factorial
 };
+//add, subtract, get the sum, multiply, get the power, and find the factorial
